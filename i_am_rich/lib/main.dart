11f6a526bbe69591bc24cd07882aff92baaaaf04
AhmedA005 <ahmed.ahmedmostafa.h@gmail.com>
2024-08-21 19:35:08 +0400

    added images

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -5,11 +5,18 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[600],
+          title: const Text(
+            'I Am Rich',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.blueGrey[800],
           centerTitle: true,
         ),
         backgroundColor: Colors.blueGrey[700],
+        body: const Center(
+            child: Image(
+          image: AssetImage('images/diamond.png'),
+        )),
       ),
     ),
   );
